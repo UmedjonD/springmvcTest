aea6a013d32b3225bd721fae6b3f98e1cac3bfad
Umedjon <Lazerr_94@mail.ru>
2021-03-09 20:07:13 +0300

    Initial commit

---
+++ src/main/java/com/davlatov/spring/mvc/MyController.java
@@ -0,0 +1,16 @@
+package com.davlatov.spring.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String showView() {
+        return "index";
+    }
+
+}
